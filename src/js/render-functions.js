1fc22fcd80235b07daca8a2c2812d6b95803ea35
Xandr <ravnb1989@gmail.com>
2024-11-05 16:43:08 +0200

    some hw ajustments 11.1

---
+++ src/js/render-functions.js
@@ -0,0 +1,32 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const gallery = document.querySelector(".gallery");
+
+
+export function renderData(apiArray) {
+    return apiArray.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
+        `<li class="list-item">
+            <a href="${largeImageURL}">
+                <img class="list-item-img" src="${webformatURL}" alt="${tags}">
+                <div class="info-container">
+                    <div class="list-item-container">
+                        <h3 class="title">Likes</h3>
+                        <p class="text">${likes}</p>
+                    </div>
+                    <div class="list-item-container">
+                        <h3 class="title">Views</h3>
+                        <p class="text">${views}</p>
+                    </div>
+                    <div class="list-item-container">
+                        <h3 class="title">Comments</h3>
+                        <p class="text">${comments}</p>
+                    </div>
+                    <div class="list-item-container">
+                        <h3 class="title">Downloads</h3>
+                        <p class="text">${downloads}</p>
+                    </div>
+                </div>
+            </a>
+        </li>
+    `).join("");
+}
---
+++ src/main.js
@@ -0,0 +1,26 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import { fetchData } from "./js/pixabay-api";
+import { renderData } from "./js/render-functions";
+
+
+const form = document.querySelector(".form");
+const loader = document.querySelector(".loader");
+const gallery = document.querySelector(".gallery");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const requestValue = event.currentTarget.elements.request.value;
+
+  fetchData(requestValue)
+    .then((data) => {
+        console.log(data);
+        gallery.innerHTML = renderData(data.hits);
+    })
+    .catch((data) => {
+      console.log(data);
+      
+    })
+}
